55a2cc6683eac9e1228d8432d2d7365883593c1e
Marius <mariusae@me.com>
2023-05-17 20:43:27 +0400

    Initial Commit

---
+++ Meditation Timer/Meditation_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meditation_TimerApp.swift
+//  Meditation Timer
+//
+//  Created by Marius Malyshev on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Meditation_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
