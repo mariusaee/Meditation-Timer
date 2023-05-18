37503a1543c8497d2d2b4f2945591832c0624f59
Marius <mariusae@me.com>
2023-05-18 12:05:40 +0400

    TimerView

--- Meditation Timer/ContentView.swift
+++ Meditation Timer/ContentView.swift
@@ -9,13 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        TimerView()
     }
 }
 
@@ -24,3 +18,52 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct TimerView: View {
+    @State private var currentTime = 0
+    @State private var timer: Timer? = nil
+
+    var body: some View {
+        VStack {
+            Text(timeString(time: currentTime))
+                .font(.largeTitle)
+            HStack {
+                Button("Start") {
+                    self.startTimer()
+                }
+                Button("Pause") {
+                    self.pauseTimer()
+                }
+                Button("Reset") {
+                    self.resetTimer()
+                }
+            }
+        }
+    }
+
+    func timeString(time: Int) -> String {
+        let minutes = Int(time) / 60
+        let seconds = Int(time) % 60
+        return String(format: "%02i:%02i", minutes, seconds)
+    }
+
+    func startTimer() {
+        self.timer?.invalidate()
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.currentTime += 1
+        }
+    }
+
+    func pauseTimer() {
+        self.timer?.invalidate()
+        self.timer = nil
+    }
+
+    func resetTimer() {
+        self.timer?.invalidate()
+        self.timer = nil
+        self.currentTime = 0
+    }
+}
+
+
